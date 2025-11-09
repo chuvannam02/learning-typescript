ffaf7e396b2c235eac5da107f05313474aea293f
Chu Van Nam <11202660@st.neu.edu.vn>
2025-11-09 22:20:40 +0700

    chore: add path alias typescript

---
+++ src/excercises/conditional-types/1.get-type-from-promise-problem.ts
@@ -0,0 +1,14 @@
+import type { Equal } from "@/type-utils";
+
+type X = Promise<string>;
+type Y = Promise<{ field: number }>;
+
+type ResultX = Transform<X>;
+type ResultY = Transform<Y>;
+
+type Transform<A> = unknown;
+
+type tests = [
+  Expect<Equal<ResultX, string>>,
+  Expect<Equal<ResultY, { field: number }>>
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths"; // <--- thêm dòng này
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+    tsconfigPaths(), // <--- giúp Vite tự đọc alias từ tsconfig.json
   ],
-})
+});
