02ffe55e5dca0d68b6910c51d4bedbfbb138ae0f
Chu Van Nam <11202660@st.neu.edu.vn>
2025-11-09 22:07:23 +0700

    docs: add description for solution of exercise 6 basic-types

--- src/excercises/basic-types/6.as-const-solution.ts
+++ src/excercises/basic-types/6.as-const-solution.ts
@@ -8,4 +8,10 @@ export const programingLanguages = {
   RUBY: "ruby",
 } as const;
 
+// Nếu không sử dụng as const thì hoàn toàn chúng ta có thể sửa được object
+// programingLanguages.JAVASCRIPT = "Something else";
+// Vì thế nên việc sử dụng (typeof programmingLanguages)["JAVASCRIPT"] sẽ trả về type là string
+// do không phải là hẳng số nên trả về string là đúng rồi.
+// => Cần không cho phép sửa giá trị trong các properties của object => chỉ đọc
+// => Sử dụng as const
 export type JavascriptLanguage = (typeof programingLanguages)["JAVASCRIPT"];
